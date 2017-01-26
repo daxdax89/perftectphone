fb5c50720f4b534ff356f5199f83a2bf5c14b11b
daxdax89 <daxdax89@msn.com>
2017-01-26 12:41:40 +0100

    bla

--- src/main/java/com/mycompany/perfectphone/MyUI.java
+++ src/main/java/com/mycompany/perfectphone/MyUI.java
@@ -1,23 +1,15 @@
 package com.mycompany.perfectphone;
 
+import com.vaadin.annotations.PreserveOnRefresh;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.navigator.Navigator;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser
@@ -30,53 +22,19 @@ import com.vaadin.ui.themes.ValoTheme;
  */
 @Theme("mytheme")
 @Title("Perfect Phone")
+@PreserveOnRefresh
 public class MyUI extends UI {
 
-    Navigator nav;
+    Navigator nav = new Navigator(this, this);
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        nav.addView("kreni", new kreni());
-        nav.navigateTo("kreni");
+        nav.addView("start", new start());
+        nav.navigateTo("start");
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
-    }
-
-    public static class kreni extends VerticalLayout implements View {
-
-        Navigator nav;
-
-        public kreni() {
-            Label title = new Label("Find Your Perfect Smartphone");
-            Label welcome = new Label("Welcome to DaX's Perfect Smartphone Finder");
-            Button start = new Button("Start");
-            start.setIcon(FontAwesome.SIGN_IN);
-
-            start.addStyleName(ValoTheme.BUTTON_PRIMARY);
-
-            Label question1 = new Label("Do you like big screen?");
-            OptionGroup answer1 = new OptionGroup();
-            Button next = new Button("Continue");
-
-            addComponents(title, question1, answer1, next);
-
-            setComponentAlignment(title, Alignment.TOP_CENTER);
-
-            setComponentAlignment(question1, Alignment.TOP_CENTER);
-
-            setComponentAlignment(answer1, Alignment.TOP_CENTER);
-
-            setComponentAlignment(next, Alignment.TOP_CENTER);
-
-        }
-
-        @Override
-        public void enter(ViewChangeListener.ViewChangeEvent event) {
-            nav = event.getNavigator();
-        }
-
     }
 }
--- src/main/java/com/mycompany/perfectphone/start.java
+++ src/main/java/com/mycompany/perfectphone/start.java
@@ -5,10 +5,52 @@
  */
 package com.mycompany.perfectphone;
 
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.OptionGroup;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
 /**
  *
  * @author daxdax89
  */
-class start {
-    
+class start extends VerticalLayout implements View {
+
+    Navigator navigator;
+
+    public start() {
+
+        Label title = new Label("Find Your Perfect Smartphone");
+        title.setId("title");
+        Label welcome = new Label("Welcome to DaX's Perfect Smartphone Finder");
+        welcome.setId("welcome");
+        Button start = new Button("Start");
+
+        start.setIcon(FontAwesome.SIGN_IN);
+
+        start.addStyleName(ValoTheme.BUTTON_PRIMARY);
+
+        Label question1 = new Label("Do you like big screen?");
+        OptionGroup answer1 = new OptionGroup();
+        Button next = new Button("Continue");
+
+        addComponents(title, question1, answer1, next);
+
+        setComponentAlignment(question1, Alignment.TOP_CENTER);
+
+        setComponentAlignment(answer1, Alignment.TOP_CENTER);
+
+        setComponentAlignment(next, Alignment.TOP_CENTER);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        navigator = event.getNavigator();
+    }
 }
